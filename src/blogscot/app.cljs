b6aa729020ead712067048257ec9129ba70610fa
Iain Diamond <iain.diamond@gmail.com>
2022-05-10 13:08:27 +0100

    Blockify the canvas

--- src/blogscot/app.cljs
+++ src/blogscot/app.cljs
@@ -1,29 +1,28 @@
 (ns blogscot.app
-  (:require [blogscot.snake :refer [draw direction move! snake]]))
+  (:require [blogscot.snake :refer [draw-snake draw-apple direction generate-apple move! snake game-dimensions]]))
 
 (def canvas (js/document.querySelector "canvas"))
 (def ctx (.getContext canvas "2d"))
-(def canvas-dimensions {:width 600 :height 400})
+(def refresh-rate 200) ;; milliseconds
 
-(def bounded-move! (move! canvas-dimensions))
+(def bounded-move! (move! game-dimensions))
 
-(defn log [arg]
-  (js/console.log (clj->js arg)))
-
-(defn draw-canvas [{:keys [width height]}]
+(defn draw-canvas [{:keys [width height size]}]
   (doto canvas
-    (#(set! (.-width %) width))
-    (#(set! (.-height %) height))
+    (#(set! (.-width %) (* width size)))
+    (#(set! (.-height %) (* height size)))
     (#(set! (.-style %) "background-color: #333"))))
 
 (defn clear-canvas []
-  (let [{:keys [width height]} canvas-dimensions]
-    (.clearRect ctx 0 0 width height)))
+  (let [{:keys [width height size]} game-dimensions]
+    (.clearRect ctx 0 0 (* width size) (* height size))))
 
 (defn game-loop []
   (bounded-move! snake)
   (clear-canvas)
-  (draw ctx @snake))
+  (draw-apple ctx)
+
+  (draw-snake ctx @snake))
 
 (defn handle-keypress [e]
   (case (-> e .-key)
@@ -34,14 +33,15 @@
     nil))
 
 (defn game []
-  (draw-canvas canvas-dimensions)
+  (draw-canvas game-dimensions)
 
   (js/document.addEventListener
    "keyup"
    handle-keypress)
 
+  (generate-apple game-dimensions)
   (game-loop)
-  (js/setInterval (fn [] (game-loop)) 50))
+  (js/setInterval game-loop refresh-rate))
 
 (defn ^:export init []
   (game))
--- src/blogscot/snake.cljs
+++ src/blogscot/snake.cljs
@@ -1,9 +1,19 @@
 (ns blogscot.snake)
 
-(def snake (atom {:x 0 :y 0 :dx 1 :dy 0}))
+(def game-dimensions {:width 60 :height 40 :size 10})
 
-(defn log [arg]
-  (js/console.log (clj->js arg)))
+(def snake (atom {:x 0 :y 0 :dx 1 :dy 0}))
+(def apple (atom {:x 0 :y 0 :color "#fff" :visible true}))
+
+(defn log [& args]
+  (js/console.log (clj->js args)))
+
+(defn point->rect
+  "Converts a [x y] point into a screen rectangle 
+   [screen-x screen-y width height]"
+  [x y]
+  (let [block-size (:size game-dimensions)]
+    [(* x block-size) (* y block-size) block-size block-size]))
 
 (defn move! [{:keys [width height]}]
   (fn [snake]
@@ -11,14 +21,39 @@
       (when-not (or
                  (and (neg? dx) (zero? x))
                  (and (neg? dy) (zero? y))
-                 (and (pos? dx) (= x (- width 10)))
-                 (and (pos? dy) (= y (- height 10))))
+                 (and (pos? dx) (= x (dec width)))
+                 (and (pos? dy) (= y (dec height))))
         (swap! snake assoc :x (+ x dx) :y (+ y dy))))))
 
-(defn draw [ctx {:keys [x y]}]
-  (set! (.-fillStyle ctx) "#fff")
-  (.fillRect ctx x y 10 10)
-  (.stroke ctx))
+(defn generate-apple [{:keys [width height]}]
+  (let [x (rand-int (dec width))
+        y (rand-int (dec height))
+        color (rand-nth ["red" "green"])]
+    (swap! apple assoc :x x :y y :color color)))
+
+(defn draw [ctx x y color]
+  (let [[rx ry w h] (point->rect x y)]
+    (set! (.-fillStyle ctx) color)
+    (.fillRect ctx rx ry w h)
+    (.stroke ctx)))
+
+(defn draw-snake [ctx {:keys [x y]}]
+  (draw ctx x y "#fff"))
+
+(defn draw-apple [ctx]
+  (let [{:keys [x y color visible]} @apple]
+    (when (true? visible)
+      (draw ctx x y color))))
 
 (defn direction [{:keys [dx dy]}]
   (swap! snake assoc :dx dx :dy dy))
+
+(comment
+
+  (def apple {:x 10 :y 20})
+
+  (let [{dx :x dy :y} apple]
+    [dx dy])
+
+
+  nil)
